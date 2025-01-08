c852eb4b9c1cacdb5b1fb07b6709a03df37a1904
Nurin Izzati <117788773+inurinizzati@users.noreply.github.com>
2025-01-08 11:25:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,3 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './', // Ensures correct relative paths
 });
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+});
